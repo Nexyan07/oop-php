423f4ea9aee41f37838ca35e529a8f1c58b5e9e0
Anto <anto32299@gmail.com>
2024-12-24 16:49:07 +0800

    inheritance

--- index.php
+++ index.php
@@ -1,26 +1,19 @@
 <?php
 
 class Siswa {
-    public $nama, $kelas, $organisasi, $bahasaPemrograman, $type;
+    public $nama, $kelas, $type;
 
-    public function __construct($nama = "unknown", $kelas = "unknown", $organisasi = null, $bahasaPemrograman = null, $type = null) {
+    public function __construct($nama = "unknown", $kelas = "unknown") {
         $this->nama = $nama;
         $this->kelas = $kelas;
-        $this->organisasi = $organisasi;
-        $this->bahasaPemrograman = $bahasaPemrograman;
-        $this->type = $type;
     }
 
     public function getLabel() {
        return "$this->nama, $this->kelas"; 
     }
-    public function getInfoLengkap() {
-        $str = "{$this->type} : {$this->getLabel()}";
-        if($this->type === "anak autodidak") {
-            $str .= " ~ {$this->bahasaPemrograman}";
-        } else if($this->type === "anak sosial") {
-            $str .= " ~ {$this->organisasi}";
-        }
+    
+    public function getInfoSiswa() {
+        $str = "Siswa = " . $this->getLabel();
         return $str;
     }
 }
@@ -31,8 +24,38 @@ class CetakInfoSiswa {
         return $str;
     }
 }
-$siswa1 = new Siswa("Anto", "XII RPL", null, "php", "anak autodidak");
-$siswa2 = new Siswa("Nabila", "XII TJKT A", "PMR", null, "anak sosial");
+
+class anakSosial extends Siswa {
+    public $organisasi;
+
+    public function __construct($nama = "unknown", $kelas = "unknown", $organisasi = null) {
+        parent::__construct($nama, $kelas);
+        $this->organisasi = $organisasi;
+    }
+
+    public function getInfoSiswa() {
+        $str = "Siswa = " . parent::getInfoSiswa() . " ~ " . $this->organisasi;
+        return $str;
+    }
+}
+
+class anakAutodidak extends Siswa {
+    public $bahasaPemrograman;
+
+    public function __construct($nama = "unknown", $kelas = "unknown", $bahasaPemrograman = null) {
+        parent::__construct($nama, $kelas);
+        $this->bahasaPemrograman = $bahasaPemrograman;
+    }
+
+    public function getInfoSiswa() {
+        $str = "Siswa = " . parent::getInfoSiswa() . " ~ " . $this->bahasaPemrograman;
+        return $str;
+    }
+}
+
+
+$siswa1 = new anakAutodidak("Anto", "XII RPL", "php");
+$siswa2 = new anakSosial("Nabila", "XII TJKT A", "PMR");
 $siswa3 = new Siswa();
 echo "nama = " . $siswa1->nama . "<br>" . "kelas = " . $siswa1->kelas;
 echo "<br>";
@@ -40,7 +63,11 @@ echo "nama = " . $siswa2->nama . "<br>" . "kelas = " . $siswa2->kelas;
 echo "<br>";
 echo "nama = " . $siswa3->nama . "<br> kelas = " . $siswa3->kelas;
 echo "<br>";
-echo $siswa2->getInfoLengkap();
+echo $siswa1->getInfoSiswa();
+echo "<br>";
+echo $siswa2->getInfoSiswa();
+echo "<br>";
+echo $siswa3->getInfoSiswa();
 echo "<br>";
 $infoSiswa1 = new CetakInfoSiswa();
 echo $infoSiswa1->cetak($siswa1);
